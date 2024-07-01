a35cb15cadfe8b1d0946fe11215c72b5cf7852e8
abuzeroyan <abuzeroyan@hotmail.com>
2024-07-01 13:09:54 +0300

    Custom Scrollbar Project Added

--- SLİDERAPP/slider.js
+++ SLİDERAPP/slider.js
@@ -1,30 +1,32 @@
+let correntIndex = 0; // Geçerli görüntünün indeksini tutar
+const slider = document.querySelector('.slider'); // Slider container'ını seçer
+const prevButton = document.getElementById('prevButton'); // "Önceki" butonunu seçer
+const nextButton = document.getElementById('nextButton'); // "Sonraki" butonunu seçer
+const images = slider.querySelectorAll('img'); // Slider içindeki tüm görüntüleri seçer
+const imageCount = images.length; // Görüntülerin sayısını alır
 
-let correntIndex = 0;
-const slider = document.querySelector('.slider')
-const prevButton = document.getElementById('prevButton')
-const nextButton = document.getElementById('nextButton')
-const images = slider.querySelectorAll('img')
-const imageCount = images.length;
+// "Önceki" butonuna tıklanıldığında showPrevImage işlevini çağırır
+prevButton.addEventListener('click', showPrevImage); 
+// "Sonraki" butonuna tıklanıldığında showNextImage işlevini çağırır
+nextButton.addEventListener('click', showNextImage);
 
-
-prevButton.addEventListener('click', showPrevImage)
-nextButton.addEventListener('click', showNextImage)
-
+// Önceki görüntüyü gösterir
 function showPrevImage(){
- correntIndex = (correntIndex - 1 + imageCount) % imageCount;
- updateSlider();
+    correntIndex = (correntIndex - 1 + imageCount) % imageCount; // Geçerli indeksi bir azaltır, gerekirse döngüsel olarak en son görüntüye gider
+    updateSlider(); // Slider'ı günceller
 }
 
+// Sonraki görüntüyü gösterir
 function showNextImage(){
-    correntIndex = (correntIndex + 1) % imageCount;
-    updateSlider();
-
+    correntIndex = (correntIndex + 1) % imageCount; // Geçerli indeksi bir artırır, gerekirse döngüsel olarak ilk görüntüye gider
+    updateSlider(); // Slider'ı günceller
 }
 
+// Slider'ı günceller ve geçerli görüntüyü gösterir
 function updateSlider(){
-    const slideWidth = 300;
-    const offset = -correntIndex * slideWidth;
-    slider.style.transform = `translateX(${offset}px)`
+    const slideWidth = 300; // Bir görüntünün genişliğini piksel cinsinden belirler
+    const offset = -correntIndex * slideWidth; // Geçerli indekse göre kaydırma mesafesini hesaplar
+    slider.style.transform = `translateX(${offset}px)`; // Slider'ı hesaplanan mesafe kadar yatayda kaydırır
 }
 
-updateSlider();
+updateSlider(); // Sayfa yüklendiğinde başlangıçta slider'ı günceller
