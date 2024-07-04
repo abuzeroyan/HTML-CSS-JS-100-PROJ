7a54e6364f933cd032abc8a3ec66324b556136f3
abuzeroyan <abuzeroyan@hotmail.com>
2024-07-04 15:41:49 +0300

    Animated-Dropdown-Menu

--- Animated-Dropdown-Menu/app.js
+++ Animated-Dropdown-Menu/app.js
@@ -1,11 +1,11 @@
-const open = document.getElementById("open");
-const close = document.getElementById('close')
-const container = document.querySelector(".container");
+const open = document.getElementById("open"); // "open" id'sine sahip elementi seçer
+const close = document.getElementById('close'); // "close" id'sine sahip elementi seçer
+const container = document.querySelector(".container"); // "container" sınıfına sahip elementi seçer
 
-open.addEventListener("click" , () =>{
-    container.classList.add("show-nav");
-})
+open.addEventListener("click", () => { 
+    container.classList.add("show-nav"); // "open" elementi tıklandığında "container" elementine "show-nav" sınıfını ekler
+});
 
 close.addEventListener("click", () => {
-    container.classList.remove("show-nav")
-})
+    container.classList.remove("show-nav"); // "close" elementi tıklandığında "container" elementinden "show-nav" sınıfını kaldırır
+});
